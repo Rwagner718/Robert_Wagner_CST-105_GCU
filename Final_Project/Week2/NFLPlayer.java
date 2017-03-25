842be0bd53d9b6abf3e8ddb85edb9cb847537c76
Robert C Wagner <usmcwookie26@gmail.com>
2017-03-25 02:56:52 -0700

    Add files via upload

---
+++ Final_Project/Week2/NFLPlayer.java
@@ -0,0 +1,26 @@
+import javafx.util.*;
+import java.util.*;
+
+public class NFLPlayer {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String player = new String();
+		String position = new String();
+		
+		System.out.print("View player by name (1) or position (2): ");
+		int choice1 = input.nextInt();
+		if(choice1 == 1) 
+		System.out.println("Tom Brady (1)");
+		System.out.println("Tom Brady (1)");
+		System.out.println("Tom Brady (1)");
+		System.out.println("Tom Brady (1)");
+		System.out.println("Tom Brady (1)");
+		
+		
+		
+		
+
+	}
+
+}
